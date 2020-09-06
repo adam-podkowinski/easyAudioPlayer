620d4b838bdb0eedf4c0b65d86ee9c3804d3f326
adampodkowinski <adampodkdev@gmail.com>
2020-09-06 16:14:56 +0200

    Remove unused code

--- uiFunctions.js
+++ uiFunctions.js
@@ -1,14 +1,5 @@
 .pragma library
 
-String.prototype.insert = function (index, string) {
-    if (index > 0) {
-        return this.substring(0, index) + string + this.substring(index,
-                                                                  this.length)
-    }
-
-    return string + this
-}
-
 function clickColor(color, change) {
 
     let hexColor = color.toString().substring(1)
